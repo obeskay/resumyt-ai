ed6fc830af5312abbb36ae5cd1b6e78954c0f95e
Obed Vargas <obeskay.mail@gmail.com>
2024-08-20 19:06:59 -0600

    feat: add initial page component

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '../components/MainLayout'
+import { Toaster } from "@/components/ui/toaster"
+
+export default function Home() {
+  return (
+    <>
+      <MainLayout />
+      <Toaster />
+    </>
+  )
+}
