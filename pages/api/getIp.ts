65df965b599cf918f84fc102292ba09f848ea4e3
Obed Vargas <obeskay.mail@gmail.com>
2024-09-02 21:56:20 -0600

    cors

--- pages/api/getIp.ts
+++ pages/api/getIp.ts
@@ -1,7 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import requestIp from "request-ip";
+import Cors from 'cors';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+// Inicializamos el middleware de CORS
+const cors = Cors({
+  origin: 'https://resumyt.com',
+  methods: ['GET', 'HEAD'],
+});
+
+// Función auxiliar para ejecutar el middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Ejecutamos el middleware de CORS
+  await runMiddleware(req, res, cors);
+
   const detectedIp =
     requestIp.getClientIp(req) ||
     req.headers["x-forwarded-for"] ||
